7999f7375131f86c6cb80349347fd0e4eeb15d4a
Bruno Gomes <bruno.rodrigues.gomes@hotmail.com>
2025-02-08 22:16:26 -0300

    Adição de imagem de fundo da homepage

--- src/paginas/Home.js
+++ src/paginas/Home.js
@@ -1,18 +1,29 @@
-import React from 'react';
-import { Link } from 'react-router-dom'
-import Header from '../componentes/Header';
+import React from "react";
+import { Link } from "react-router-dom";
+import Header from "../componentes/Header";
 
-function Home(){
-    return(
-        <div>
-            <Header/>
-            <h1>Home</h1>
-            <p>Bem vindo a pagina inicial</p>
-            <Link to={"/contato"}>
-            Ir para contato
-            </Link>
+function Home() {
+  return (
+    <div>
+      <Header />
+      <div className="div-home">
+        <div className="conteudo-home">
+          <div className="titulo-home">
+            <h1>OLÁ, SOU O BRUNO RODRIGUES</h1>
+          </div>
+          <div className="descricao-home">
+          <p>Desenvolvedor front end que sabe bastante sobre Angular, alguma coisa sobre Vue e está iniciando seus estudos em React</p>
+          </div>
+          <button className="botao-home">Projetos</button>
         </div>
-    )
+      </div>
+      <div className="div-sobre">
+        <div className="conteudo-sobre">
+            <p>SOBRE</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
