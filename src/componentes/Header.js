f4de08ce7074712c9ded354abfe37b294378afbb
Bruno Gomes <bruno.rodrigues.gomes@hotmail.com>
2025-02-08 22:16:01 -0300

    Adição do avatar

--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -5,6 +5,8 @@ function Header(){
     return(
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
+        <img src="avatar-bruno.png" className="rounded-circle avatar" 
+  alt="Avatar" />
           <a class="navbar-brand" href="#">Bruno Rodrigues</a>
           {/* <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
